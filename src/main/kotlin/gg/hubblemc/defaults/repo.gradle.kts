9bd441fc49380bf2dd1e3172ad49f15bdba3fd9f
Koding Dev <kodingdev@pm.me>
2022-10-27 15:45:17 +1100

    fix(repo): add custom repos at the end

--- src/main/kotlin/gg/hubblemc/defaults/repo.gradle.kts
+++ src/main/kotlin/gg/hubblemc/defaults/repo.gradle.kts
@@ -2,22 +2,24 @@ package gg.hubblemc.defaults
 
 import gg.hubblemc.util.authenticatedMaven
 
-repositories {
-    mavenCentral()
+afterEvaluate {
+    repositories {
+        mavenCentral()
 
-    // Koding.dev - Public
-    maven("https://repo.koding.dev/releases/")
-    maven("https://repo.koding.dev/snapshots/")
+        // Koding.dev - Public
+        maven("https://repo.koding.dev/releases/")
+        maven("https://repo.koding.dev/snapshots/")
 
-    // Hubble - Authenticated
-    // To use this, you must set either
-    // Properties: hubble.username, hubble.password
-    // Environment: HUBBLE_USERNAME, HUBBLE_PASSWORD
-    authenticatedMaven("https://repo.koding.dev/hubble-releases/", "hubble", project) {
-        name = "HubbleReleases"
-    }
+        // Hubble - Authenticated
+        // To use this, you must set either
+        // Properties: hubble.username, hubble.password
+        // Environment: HUBBLE_USERNAME, HUBBLE_PASSWORD
+        authenticatedMaven("https://repo.koding.dev/hubble-releases/", "hubble", project) {
+            name = "HubbleReleases"
+        }
 
-    authenticatedMaven("https://repo.koding.dev/hubble-snapshots/", "hubble", project) {
-        name = "HubbleSnapshots"
+        authenticatedMaven("https://repo.koding.dev/hubble-snapshots/", "hubble", project) {
+            name = "HubbleSnapshots"
+        }
     }
 }
